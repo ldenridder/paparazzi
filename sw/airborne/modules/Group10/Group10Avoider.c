4b48a1bd9f4f745ebaf4135e532f23396a600ccd
habujurji <71014563+habujurji@users.noreply.github.com>
2021-03-21 14:41:38 +0100

    Add files via upload

--- sw/airborne/modules/Group10/Group10Avoider.c
+++ sw/airborne/modules/Group10/Group10Avoider.c
@@ -1,27 +1,18 @@
 #include "modules/project/Group10Avoider.h"
+#include "subsystems/abi.h"
 
-#ifndef PROCESS_FPS
-#define PROCESS_FPS 0       ///< Default FPS (zero means run at camera fps)
-#endif
+static abi_event allowable_distance_ev;
+static void allowable_distance_cb(uint8_t __attribute__((unused)) sender_id,
+                               int32_t __attribute__((unused)) allowableDistance)
+{
+	navInput = allowableDistance; //Pointer to a vector containing the allowable distances in each lane
+}
 
 void avoiderInit(void){
-	listener = cv_add_to_device(&VIDEO_CAPTURE_CAMERA, imageProcess, PROCESS_FPS); //Check &COLORFILTER_CAMER!!!!!!!!!!!!!!!
+	AbiBindMsgNAVIGATION_VECTOR(NAVIGATION_VECTOR_ID, &allowable_distance_ev, allowable_distance_cb);
 }
 
 void avoiderPeriodic(void){
-	grad_x(img,x_grad,X,Y);
-	grad_y(img,y_grad,X,Y);
-	grad_x(x_grad,xx_grad,X,Y);
-	grad_y(y_grad,yy_grad,X,Y);
-	grad_y(x_grad,xy_grad,X,Y);
-	shape_ind(xx_grad,yy_grad,xy_grad,shape_index,X,Y);
-	hmat_z_func(img,shape_index,hmat_z,-0.5,X,Y);
-	hmat_z_func(img,shape_index,hmat_z,0.5,X,Y);
-	hmat_z_func(img,shape_index,hmat_z,0,X,Y);
-	noiseFilter(hmat_z,X,Y);
-	grid_counter(img,grid,n_rows,n_columns,grid_height,grid_width,X);
-	output_conversion(grid,navInput,n_columns,n_rows);
-	
-	//Navigation
 	
 }
+
